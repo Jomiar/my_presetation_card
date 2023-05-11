679ef58996f5a4551e75f012d0d021c53b91d7d6
Jomiar <jossedar1990@icloud.com>
2023-05-11 14:26:47 -0500

    fead: /add image add name

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hola Mundo desde Flutter'),
+        backgroundColor: Colors.indigo,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  backgroundImage: AssetImage("assets/imagen 1.png"),
+                  radius: 80,
+                ),
+                const SizedBox(height: 24),
+                Card(
+                  elevation: 40,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: const [
+                        Text('Jose Dario Millan Aranibar'),
+                        Icon(Icons.flutter_dash)
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
